ef8219967a12314f31dbe7ccd82935c5df151d92
Thirza van Hemert <cahemert@avans.nl>
2020-04-07 15:09:14 +0200

    Added Movie Class

---
+++ src/model/Movie.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Movie extends Video{
+
+    private String language;
+    private String genre;
+    private int ageIndication;
+
+    public Movie(int id, String title, String duration, String genre, String language, int ageIndication){
+        super(id, title, duration);
+        this.genre = genre;
+        this.language = language;
+        this.ageIndication = ageIndication;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getAgeIndication() {
+        return ageIndication;
+    }
+
+    public void setAgeIndication(int ageIndication) {
+        this.ageIndication = ageIndication;
+    }
+}
