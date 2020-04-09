82b1acbdf8b6cd3e32a86873e7647e795410d276
Thirza van Hemert <cahemert@avans.nl>
2020-04-09 13:56:00 +0200

    Added DatabaseConnection functionality

--- src/Database/DatabaseConnection.java
+++ src/Database/DatabaseConnection.java
@@ -1,4 +1,25 @@
 package Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DatabaseConnection {
+    private final String connectionUrl = "jdbc:sqlserver://localhost\\MSSQLSERVER;databaseName=NetflixStatistix;integratedSecurity=true;";
+    private Connection con = null;
+
+    public DatabaseConnection() {
+        try {
+            // 'Importeer' de driver die je gedownload hebt.
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            // Maak de verbinding met de database.
+            con = DriverManager.getConnection(connectionUrl);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getCon() {
+        return con;
+    }
 }
