497f96c2d019aa10616f3b9418b3d2ecb56e9226
Thirza van Hemert <cahemert@avans.nl>
2020-04-09 19:05:52 +0200

    Changed DatabaseConnection class

--- src/Database/DatabaseConnection.java
+++ src/Database/DatabaseConnection.java
@@ -2,24 +2,51 @@ package Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
 
 public class DatabaseConnection {
+    private static DatabaseConnection instance;
     private final String connectionUrl = "jdbc:sqlserver://localhost\\MSSQLSERVER;databaseName=NetflixStatistix;integratedSecurity=true;";
-    private Connection con = null;
+    private static Connection con = null;
 
-    public DatabaseConnection() {
-        try {
-            // 'Importeer' de driver die je gedownload hebt.
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            // Maak de verbinding met de database.
-            con = DriverManager.getConnection(connectionUrl);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            return new DatabaseConnection();
+        } else {
+            return instance;
         }
     }
 
-    public Connection getCon() {
+    private DatabaseConnection(){
+
+    }
+
+    public Connection connect() {
+        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager
+                    .getConnection(connectionUrl);
+            return connection;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+//    public DatabaseConnection() {
+//        try {
+//            // 'Importeer' de driver die je gedownload hebt.
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            // Maak de verbinding met de database.
+//            con = DriverManager.getConnection(connectionUrl);
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    public static Connection getCon() {
         return con;
     }
 }
